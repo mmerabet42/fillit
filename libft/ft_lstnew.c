d02a5c08a8b010720827cf440360953ee735784f
Mohammed MERABET <mmerabet@e2r11p21.42.fr>
2017-11-16 17:45:15 +0100

    42

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/11 17:31:55 by mmerabet          #+#    #+#             */
+/*   Updated: 2017/11/12 18:53:08 by mmerabet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(const void *content, size_t content_size)
+{
+	t_list	*lst;
+
+	if ((lst = (t_list *)ft_memalloc(sizeof(t_list))))
+	{
+		if (content)
+			lst->content = ft_memdup(content, content_size);
+		lst->content_size = (content ? content_size : 0);
+	}
+	return (lst);
+}
