4d15be924d5bea4628eab1a09921dab25ab8d723
Mohammed MERABET <mmerabet@e1r2p7.42.fr>
2017-11-20 00:01:08 +0100

    l

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 22:41:21 by mmerabet          #+#    #+#             */
-/*   Updated: 2017/11/17 23:25:05 by mmerabet         ###   ########.fr       */
+/*   Updated: 2017/11/19 13:12:12 by mmerabet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ typedef struct	s_mapdata
 {
 	int			size;
 	char		**arr;
+	t_list		*tetris;
 }				t_mapdata;
 
 typedef int t_bool;
@@ -42,8 +43,9 @@ void	ft_fillit(int fd);
 
 t_list		*ft_get_tetris(int fd);
 t_mapdata	*ft_solve_tetris(t_list *tetris);
-void		ft_map_tetris(t_list *lst, t_mapdata *mapdata);
+void		ft_map_tetris(t_mapdata *mapdata);
 void		ft_print_map(t_mapdata *mapdata);
-
+t_tetri		ft_new_tetri(char c, int x, int y, int p0x, int p0y,
+					int p1x, int p1y, int p2x, int p2y, int p3x, int p3y);
 
 #endif
