54f0bf7cc669857f7e385d580da3dfd0d52c0667
Victor TENNERONI <vtennero@student.42.fr>
2017-11-20 21:13:48 +0100

    toto

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: mmerabet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/15 22:41:21 by mmerabet          #+#    #+#             */
-/*   Updated: 2017/11/19 13:12:12 by mmerabet         ###   ########.fr       */
+/*   Updated: 2017/11/20 21:13:40 by vtennero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ typedef struct	s_mapdata
 {
 	int			size;
 	char		**arr;
-	t_list		*tetris;
 }				t_mapdata;
 
 typedef int t_bool;
@@ -42,10 +41,13 @@ typedef int t_bool;
 void	ft_fillit(int fd);
 
 t_list		*ft_get_tetris(int fd);
+t_list		*ft_read_tetris(int fd, char buf[6], int buffsize, int i);
+t_bool		ft_build_tetris(t_list *lst);
+t_bool		ft_valid_tetris(t_list *lst);
+t_list		*ft_absolute_tetris(t_list *lst);
 t_mapdata	*ft_solve_tetris(t_list *tetris);
-void		ft_map_tetris(t_mapdata *mapdata);
+void		ft_map_tetris(t_list *lst, t_mapdata *mapdata);
 void		ft_print_map(t_mapdata *mapdata);
-t_tetri		ft_new_tetri(char c, int x, int y, int p0x, int p0y,
-					int p1x, int p1y, int p2x, int p2y, int p3x, int p3y);
+
 
 #endif
