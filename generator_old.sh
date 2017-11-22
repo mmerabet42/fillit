90b9fa089f91cc85201a755c2ecbe2ba30868830
Victor TENNERONI <vtennero@student.42.fr>
2017-11-22 18:51:06 +0100

    titi

--- generator_old.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-c=1
-n=100
-while [ $c -le $n ]
-do
-	perl generator.perl $(( ( RANDOM % 26 )  + 1 )) 2 > "tests/test$c"
-		echo "test$c : \c" && ./a.out "tests/test$c"
-			(( c++ ))
-		done
